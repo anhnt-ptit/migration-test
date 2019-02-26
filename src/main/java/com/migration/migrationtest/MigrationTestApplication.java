57f4fbf994a20d722a8a6f95f211d3c81685615c
anhnt53 <anhnt53@IB1-031673-1L.User.techcombank.com.vn>
2019-02-26 15:00:18 +0700

    test commit

---
+++ src/main/java/com/migration/migrationtest/MigrationTestApplication.java
@@ -0,0 +1,13 @@
+package com.migration.migrationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationTestApplication.class, args);
+	}
+
+}
